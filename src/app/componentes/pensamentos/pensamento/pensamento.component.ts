2459f73bc8b33ce65b4c3c8eaec1c0aeb750d600
unknown <mateus.ramos1337@gmail.com>
2022-09-29 16:56:25 -0300

    Adicionando diretivas para formatar css e percorrcorrer uma lista de objetos

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  //Anotação input diz que a propriedade pensamento vai receber informações do componente pai
+  @Input()
+  pensamento ={
+    conteudo: 'Eu gosto de Angular',
+    autoria: 'Mateus',
+    modelo: 'modelo3'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //iniciando uma função que retorna uma string
+  //Método para trocar a classe css de forma condicional
+  larguraPensamento(): string{
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
   }
 
 }
