86e53bd85b17018546ad698e68a6c1011768c782
unknown <mateus.ramos1337@gmail.com>
2022-09-29 15:27:56 -0300

    Configurando rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Configuração das rotas que temos na aplicação
+const routes: Routes = [
+  {
+    //Caso usuário digite somente a ulr sem uma rota, ele será redirecionado
+    path:'',
+    redirectTo:'listarPensamento',
+    pathMatch: 'full'
+  },
+  {
+    path:'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path:'listarPensamento',
+    component:ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
