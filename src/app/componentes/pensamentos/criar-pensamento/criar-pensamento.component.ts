9753dde93b6a340b6e1525021ff845edab9916e9
unknown <mateus.ramos1337@gmail.com>
2022-10-24 15:21:52 -0300

    Iniciando com FormGroups para formularios reativos

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,24 +11,25 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-
-    conteudo: '',
-    autoria: '',
-    modelo:''
-  }
+  formulario!: FormGroup;
 
 
   constructor(
     private service: PensamentoService,
-    private router : Router
+    private router : Router,
+    private formBuilder : FormBuilder
     ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'], 
+      autoria: ['Angular1'],
+      modelo:['modelo1']
+    })
   }
 
   criarPensamento(){
-    this.service.criar(this.pensamento).subscribe(()=> {
+    this.service.criar(this.formulario.value).subscribe(()=> {
       this.router.navigate(['/listarPensamento'])
     })
   }
