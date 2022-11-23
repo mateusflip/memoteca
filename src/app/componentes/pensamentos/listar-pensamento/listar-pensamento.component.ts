bfcd0510963ea6859d17ce2132c9d5275cdee3ec
unknown <mateus.ramos1337@gmail.com>
2022-11-23 16:20:50 -0300

    Funcionalidade de busca

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -13,24 +13,34 @@ export class ListarPensamentoComponent implements OnInit {
    listaPensamentos: Pensamento[]=[];
    paginaAtual: number = 1;
    haMaisPensamentos: boolean = true;
+   filtro: string = '' //Filtro que usamos no campo de busca
   constructor(private service: PensamentoService) { }
 
   //O que queremos que seja executado assim que o componente inicia
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos(){
     //Colocamos ++ ano listar pois vamos incrementar
-    this.service.listar(++this.paginaAtual)
+    this.service.listar(++this.paginaAtual, this.filtro)
     .subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
         this.haMaisPensamentos = false
       }
     })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos
+      })
   }
 
 }
